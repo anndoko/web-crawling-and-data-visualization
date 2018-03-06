199600b02bb1ba75318c37c6ade03097cfa7cb6e
Anndo Ko <handcuffanndo@gmail.com>
2018-03-06 01:41:05 -0500

    scrape the pages and get sites for state

--- proj2_nps.py
+++ proj2_nps.py
@@ -1,7 +1,8 @@
 ## proj_nps.py
 ## Skeleton for Project 2, Winter 2018
 ## ~~~ modify this file, but don't rename it ~~~
-
+import requests
+from bs4 import BeautifulSoup
 
 ## you can, and should add to and modify this class any way you see fit
 ## you can add attributes and modify the __init__ parameters,
@@ -21,6 +22,9 @@ class NationalSite():
         self.address_state = 'KS'
         self.address_zip = '11111'
 
+    def __str__(self):
+        return "{} ({}): {}, {}, {} {}".format(self.name, self.type, self.address_street, self.address_city, self.address_state, self.address_zip)
+
 ## you can, and should add to and modify this class any way you see fit
 ## you can add attributes and modify the __init__ parameters,
 ##   as long as tests still pass
@@ -37,8 +41,60 @@ class NearbyPlace():
 ##        (e.g., National Parks, National Heritage Sites, etc.) that are listed
 ##        for the state at nps.gov
 def get_sites_for_state(state_abbr):
-    return []
+    # baseurl
+    baseurl = "https://www.nps.gov"
 
+    # scrap the homepage
+    homepage_url = baseurl + "/index.htm" # set the url of the homepage
+    page_text = requests.get(homepage_url).text
+    page_soup = BeautifulSoup(page_text, "html.parser")
+
+    # get state abbrs and end nodes
+    dropdown_menu_items = page_soup.find(class_ = "dropdown-menu")
+    end_node_dic = {} # create a dic to store the abbrs of states and the end nodes
+    for item in dropdown_menu_items:
+        try:
+            end_node = item.find("a")["href"]
+            end_node_dic[end_node[7:9]] = end_node
+        except:
+            continue
+
+    # scrape the state page
+    national_sites_page_url = baseurl + end_node_dic[state_abbr] # set the url of the target page
+    sites_page_text = requests.get(national_sites_page_url).text
+    sites_page_soup = BeautifulSoup(sites_page_text, "html.parser")
+
+    # get sites
+    sites_lst_items = sites_page_soup.find(id = "list_parks")
+    results_lst = [] # create a list to store the instances of NaitonalSite
+    for site in sites_lst_items:
+        try:
+            # get the basic info: type, name, description, and url
+            site_type = site.find("h2").string
+            site_name = site.find("a").string
+            site_description = site.find("p").text
+            site_url = baseurl + site.find("a")["href"]
+
+            # create instance using the basic info as its params
+            national_site = NationalSite(site_type, site_name, site_description, site_url)
+
+            # scrape the detail page to get the address
+            detail_page_text = requests.get(site_url).text
+            detail_page_soup = BeautifulSoup(detail_page_text, "html.parser")
+            adr_items = detail_page_soup.find(class_ = "adr")
+
+            # assign the values
+            national_site.address_street = adr_items.find("span", class_ = "street-address").text.replace("\n","")
+            national_site.address_city = adr_items.find(itemprop = "addressLocality").text
+            national_site.address_state = adr_items.find(class_ = "region").text
+            national_site.address_zip = adr_items.find(class_ = "postal-code").text[0:5]
+
+            # add the instance to the list
+            results_lst.append(national_site)
+        except:
+            continue
+            
+    return results_lst
 
 ## Must return the list of NearbyPlaces for the specifite NationalSite
 ## param: a NationalSite object
