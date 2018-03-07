e3f2de097915104213220f046f43e1b1ed19e48e
Anndo Ko <handcuffanndo@gmail.com>
2018-03-07 02:18:29 -0500

    request data using Google Places API to search for nearby places

--- proj2_nps.py
+++ proj2_nps.py
@@ -1,8 +1,9 @@
 ## proj_nps.py
 ## Skeleton for Project 2, Winter 2018
-## ~~~ modify this file, but don't rename it ~~~
+## ~~~ modify this file, but don"t rename it ~~~
 import requests
 import json
+import secret
 from bs4 import BeautifulSoup
 
 ## you can, and should add to and modify this class any way you see fit
@@ -18,10 +19,10 @@ class NationalSite():
         self.url = url
 
         # needs to be changed, obvi.
-        self.address_street = '123 Main St.'
-        self.address_city = 'Smallville'
-        self.address_state = 'KS'
-        self.address_zip = '11111'
+        self.address_street = "123 Main St."
+        self.address_city = "Smallville"
+        self.address_state = "KS"
+        self.address_zip = "11111"
 
     def __str__(self):
         return "{} ({}): {}, {}, {} {}".format(self.name, self.type, self.address_street, self.address_city, self.address_state, self.address_zip)
@@ -34,6 +35,12 @@ class NationalSite():
 class NearbyPlace():
     def __init__(self, name):
         self.name = name
+        # lag & lng
+        self.lag = ""
+        self.lng = ""
+
+    def __str__(self):
+        return self.name
 
 ## Must return the list of NationalSites for the specified state
 ## param: the 2-letter state abbreviation, lowercase
@@ -131,13 +138,54 @@ def get_sites_for_state(state_abbr):
 
     return results_lst
 
+
 ## Must return the list of NearbyPlaces for the specifite NationalSite
 ## param: a NationalSite object
 ## returns: a list of NearbyPlaces within 10km of the given site
 ##          if the site is not found by a Google Places search, this should
 ##          return an empty list
+
+# ---------- Caching ----------
+
 def get_nearby_places_for_site(national_site):
-    return []
+    # set the key
+    google_api_key = secret.google_places_key
+
+    # get the GPS information using Google Geocode API
+    ## form the url, params: name, type, and key
+    google_geo_url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query={}&tyepe={}&key={}".format(national_site.name, national_site.type, google_api_key)
+    ## request the data
+    geo_results = requests.get(url = google_geo_url)
+    geo_results_py = json.loads(geo_results.text)
+    ## get the lat & lng for nearby places search
+    site_lat = geo_results_py["results"][0]["geometry"]["location"]["lat"]
+    site_lng = geo_results_py["results"][0]["geometry"]["location"]["lng"]
+
+    # get the places nearby using Google Places API
+    # for the url, params: lat, lng, radius, and key
+    google_places_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={}, {}&radius=10000&key={}".format(site_lat, site_lng, google_api_key)
+    ## requesting the data
+    nearby_results = requests.get(url = google_places_url)
+    nearby_results_py = json.loads(nearby_results.text)
+
+    # get places
+    results_lst = [] # create a list to store the instances of NearbyPlace
+    for place in nearby_results_py["results"]:
+        try:
+            # get the name
+            place_name = place["name"]
+
+            # create instance using the basic info as its param
+            nearby_place = NearbyPlace(place_name)
+            nearby_place.lat = place["geometry"]["location"]["lat"]
+            nearby_place.lng = place["geometry"]["location"]["lng"]
+
+            # add the instance to the list
+            results_lst.append(nearby_place)
+        except:
+            continue
+
+    return results_lst
 
 ## Must plot all of the NationalSites listed for the state on nps.gov
 ## Note that some NationalSites might actually be located outside the state.
